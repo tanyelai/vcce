3f4e78f0e44b946153eeb807cde9c0106ba71b62
Toygarr <toygartanyel@hotmail.com>
2023-11-26 13:13:37 +0300

    Remove redundant comments

--- vcce/utils/data.py
+++ vcce/utils/data.py
@@ -41,20 +41,19 @@ def retrieve_test_patient(path, patient_idx, output_dir):
     # Derive the output_filename based on patient_idx and label
     output_filename = f"{patient_idx}_{label}"
 
-    # Create the dataset directory
     dataset_dir = os.path.join(output_dir, output_filename)
     os.makedirs(dataset_dir, exist_ok=True)
 
     print(f"The extracted patients are saved in the following directory:\n\
           {dataset_dir}\n")
  
-    # Save train and test data to separate files
+    # save train and test data to separate files
     np.save(f"{dataset_dir}/signal.npy", signal)
     np.save(f"{dataset_dir}/label.npy", label)
     np.save(f"{dataset_dir}/info.npy", info)
 
 
-# Functions for create_dataset
+# functions for create_dataset
 def load_raw_data(df, sampling_rate, path):
     if sampling_rate == 100:
         data = [wfdb.rdsamp(path+f) for f in tqdm(df.filename_lr)]
@@ -94,7 +93,7 @@ def get_new_data(X, Y):
 
 
 def create_binary_dataset(x_data, y_data, class_labels, patient_info, random_state):
-    # Find the class with the minimum count
+    # find the class with the minimum count
     min_label_count = min(Counter(y_data).values())
 
     # Undersample the data for each class
@@ -124,14 +123,13 @@ def create_binary_dataset(x_data, y_data, class_labels, patient_info, random_sta
 def create(x_new, y_new, patient_info, dataset_dir):
     x_equalized, y_equalized, info_equalized = create_binary_dataset(x_new, y_new, ["NORM", "MI"], patient_info, random_state=0)
     
-    # Split equalized data into train and test sets
+    # split equalized data into train and test sets
     x_train, x_test, y_train, y_test, info_train, info_test = train_test_split(
         x_equalized, y_equalized, info_equalized, test_size=0.2, stratify=y_equalized, random_state=0)
 
-    # Create the dataset directory
     os.makedirs(dataset_dir, exist_ok=True)
 
-    # Save train and test data to separate files
+    # save train and test data to separate files
     np.save(f"{dataset_dir}/signals_train.npy", x_train)
     np.save(f"{dataset_dir}/labels_train.npy", y_train)
     np.save(f"{dataset_dir}/patient_info_train.npy", info_train)
@@ -139,11 +137,11 @@ def create(x_new, y_new, patient_info, dataset_dir):
     np.save(f"{dataset_dir}/labels_test.npy", y_test)
     np.save(f"{dataset_dir}/patient_info_test.npy", info_test)
 
-    # Count the occurrences of the class label in the train and test sets
+    # count the occurrences of the class label in the train and test sets
     class_counts_train = {label: int(count) for label, count in Counter(y_train).items()}
     class_counts_test = {label: int(count) for label, count in Counter(y_test).items()}
     
-    # Save train and test class counts as text (txt) files
+    # save train and test class counts as text (txt) files
     with open(f"{dataset_dir}/class_counts_train.txt", "w") as f_train:
         f_train.write(json.dumps(class_counts_train, indent=4))
         
@@ -158,7 +156,7 @@ def create_dataset(path, sampling_rate, output_dir):
     
     X = load_raw_data(Y, sampling_rate, path)
 
-    # Load scp_statements.csv for diagnostic aggregation
+    # load scp_statements.csv for diagnostic aggregation
     agg_df = pd.read_csv(os.path.join(path, 'scp_statements.csv'), index_col=0)
     agg_df = agg_df[agg_df.diagnostic == 1]
 
